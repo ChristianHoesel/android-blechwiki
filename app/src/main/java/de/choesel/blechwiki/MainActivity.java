98499562c57080c4542297b0514c6c6926ad2bc8
Christian <ChristianHoesel@users.noreply.github.com>
2016-05-08 10:44:54 +0200

    Suchfunktion in der AppBar

--- app/src/main/java/de/choesel/blechwiki/MainActivity.java
+++ app/src/main/java/de/choesel/blechwiki/MainActivity.java
@@ -1,8 +1,11 @@
 package de.choesel.blechwiki;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
@@ -12,6 +15,7 @@ import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -72,6 +76,14 @@ public class MainActivity extends AppCompatActivity implements BuchFragment.OnLi
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
         return true;
     }
 
